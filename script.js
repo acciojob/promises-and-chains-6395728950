91e5f6b1cf7e8aa8579bbd3440d55e678a973276
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-05-08 19:13:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+  document.getElementById('voteform').addEventListener("submit",function(e){
+	  e.preventDefault();
+	  const name = document.getElementById('name').value.trim();
+	  const age =  parseInt(document.getElementById('age').value);
+	  if(name ==='' || age ===''){
+		  alert("Please enter valid details");
+		  return;
+	  }
+
+	  // now your task is create a promise
+	  const promise = new Promise((resolve,reject)=>{
+		   setTimeout(()=>{
+			   if(age>=18){
+				   resolve();
+			   }else{
+				   reject();
+			   }
+		   },4000);
+	  });
+	  promise.then(()=>{
+		  alert("Welcome,.You can vote.")
+	  }).catch(()=>{
+		  alert("Oh sorry.You aren't old enough");
+	  });
+	  
+	  
+	  
+  })
+
+ 
